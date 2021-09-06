018e7bb2d2cd29c9a5ea398808fbb7a425f22913
Christian Kämpfe <kaempfechristian@protonmail.com>
2021-09-06 21:31:56 +0200

    added build.sh

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+cd "$(dirname "$0")"
+
+VERSION=''
+
+if [ -e .git ]; then
+    TAGS="$(git tag | while read i; do
+        printf '%s %s\n' \
+            "$i" \
+            "$(git rev-parse "$i")"
+    done)"
+    HEAD="$(git rev-parse HEAD)"
+    if echo "$TAGS" | grep -F "$HEAD" >/dev/null; then
+        VERSION="$(echo "$TAGS" | 
+                   grep -F "$HEAD" |
+                   sed 's|\s[0-9a-f]\+$||g')"
+    else 
+        VERSION="$(git rev-parse --short HEAD)"
+    fi
+else
+    VERSION="$(basename "$(pwd)" |
+               grep 'v[0-9.]\+$')"
+fi
+
+ALPINE_VERSION="$(cat Dockerfile |
+                  grep '^FROM' |
+                  sed 's|^FROM\s||g' |
+                  sed 's|:|-|g')"
+
+docker build -t "thosebeans/wireguard-docker:${VERSION}-${ALPINE_VERSION}" .
